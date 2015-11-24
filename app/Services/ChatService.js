c8d2189e328772d9e534a42a56143bb924706af2
IrmaJakic <ijakic1@etf.unsa.ba>
2015-11-24 03:17:32 +0100

    Fourth Commit
    Pictures changed, directive deleted

--- app/Services/ChatService.js
+++ app/Services/ChatService.js
@@ -29,16 +29,16 @@
         , "He's spending a year dead for tax reasons."];
     };
     var getImages = function () {
-        return ['http://static6.businessinsider.com/image/558d9fd36da811040de177dd-1200-900/harry-potter-daniel-radcliffe-hogwarts-wizard-teenager-teen-5.jpg',
-                 'https://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poster.jpg',
-        'http://cdn.collider.com/wp-content/uploads/2015/07/ron-weasley.jpg',
-        'http://vignette4.wikia.nocookie.net/harrypotter/images/6/6d/Luna-Lovegood-Wallpaper-luna-lovegood-25518113-1024-768.jpg/revision/latest?cb=20121028020840',
-        'http://booklikes.com/upload/post/e/7/e7655f868371049aa621fc5ae6834330.jpg',
-        'http://images.gamenguide.com/data/images/full/19524/tom-felton-as-draco-malfoy-from-harry-potter.jpg?w=720',
-        'http://data.whicdn.com/images/63543108/large.png',
-        'https://pbs.twimg.com/profile_images/476841653710626816/kID2ZqOd.jpeg',
-        'http://bloximages.newyork1.vip.townnews.com/roanoke.com/content/tncms/assets/v3/editorial/b/40/b402a776-942a-11e3-b3c3-0017a43b2370/52fbe468ec796.image.jpg',
-        'http://vignette1.wikia.nocookie.net/harrypotter/images/d/da/AngieJ.jpg/revision/latest?cb=20080829182719'
+        return ['https://s3-us-west-1.amazonaws.com/patreon.posts/12709863287529890211.jpg',
+                 'https://s3-us-west-1.amazonaws.com/patreon.posts/11464378962196459204.jpg',
+        'https://s3-us-west-1.amazonaws.com/patreon.posts/10104750268837050043.jpg',
+        'https://s3-us-west-1.amazonaws.com/patreon.posts/10662800238131123243.jpg',
+        'https://s3-us-west-1.amazonaws.com/patreon.posts/7848014498817890722.jpg',
+        'https://s3-us-west-1.amazonaws.com/patreon.posts/446308271092482862.jpg',
+        'https://s3-us-west-1.amazonaws.com/patreon.posts/2144896575646823284.jpg',
+        'https://s3-us-west-1.amazonaws.com/patreon.posts/15594870116961159127.jpg',
+        'https://s3-us-west-1.amazonaws.com/patreon.posts/3606964333439401758.jpg',
+        'https://s3-us-west-1.amazonaws.com/patreon.posts/10506545918758145056.jpg'
         ];
     };
     return {
