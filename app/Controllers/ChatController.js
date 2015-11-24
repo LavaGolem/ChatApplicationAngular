91dce3e7db999b38f3363afb36ea453006158758
IrmaJakic <ijakic1@etf.unsa.ba>
2015-11-24 05:38:47 +0100

    Fifth Commit
    Badges with unread messages added

--- app/Controllers/ChatController.js
+++ app/Controllers/ChatController.js
@@ -3,44 +3,49 @@
 
 
         //Initial activities
-        $scope.images = ChatService.getImages();
-        $scope.usersImages = [{ image: '', id: '' }];
-        $scope.otherMessages = [{ message: '', id: '', me: '' }];
-        $scope.messages = ChatService.getMessages();
-        $scope.id = 0;//id of a user whoes window is open
+        $scope.images = ChatService.getImages();  //Geting images from server for users
+        $scope.usersImages = [{ image: '', id: '' }]; //Images and users they are assigned to
+        $scope.otherMessages = [{ message: '', id: '', me: '' }];//Messages in chat room
+        $scope.messages = ChatService.getMessages(); //Getting messages from server, for users to use
+        $scope.id = 0;//id of a user whose window is open
         $scope.allMessages = [{ message: '', id: '', me: '' }]; //all messages in private windows
-        $scope.visibleList = 'hidden-xs hidden-sm'; //initial class of all contacts panel
-        $scope.visibleRoom = '';
-        $scope.visibleButton = 'glyphicon glyphicon-chevron-down pull-right glifi hidden-md hidden-lg';
-        $scope.visibleButtonRoom = 'glyphicon glyphicon-chevron-up pull-right glifi hidden-md hidden-lg';
-        $scope.down = true;
-        $scope.downRoom = false;
-        $scope.mediaClass = '';
-        $scope.cdate = new Date();
+        $scope.visibleList = 'hidden-xs hidden-sm'; //Initial class of all contacts panel
+        $scope.visibleRoom = '';//Initial class od Chat Room panel
+        $scope.visibleButton = 'glyphicon glyphicon-chevron-down pull-right glifi hidden-md hidden-lg'; //Initial class of button on private chat panel
+        $scope.visibleButtonRoom = 'glyphicon glyphicon-chevron-up pull-right glifi hidden-md hidden-lg';//Initial class of button on chat room panel
+        $scope.down = true;//Initial value of glifi button on private chat panel
+        $scope.downRoom = false; //Initial value of glifi button on chat room panel
+        $scope.mediaClass = ''; //Initial value of media class
+        $scope.cdate = new Date(); //Getting date time now
+        $scope.unreadMessages = [{ id: '', cou: 0 }]; //Unread messages in private windows
+        $scope.randomInterval = Math.floor((Math.random() * 10000) + 3000);//Random interval to be used
 
-
-        $scope.randomInterval = Math.floor((Math.random() * 10000) + 3000)
         //Get users from server
         $http.get("http://jsonplaceholder.typicode.com/users")
         .success(function (response) {
             $scope.users = response;
             //Assign images to users
             for (var i = 0 ; i < $scope.users.length; i++) {
+                //Assigning images to users 
                 $scope.usersImages.push({ image: $scope.images[i], id: $scope.users[i].id });
+                //Setting initial values to unread messages of users
+                $scope.unreadMessages.push({ id: $scope.users[i].id, cou:0 })
             }
-            $interval(function () { $scope.getNewMessages(); }, $scope.randomInterval);
-            
+            //Starting two interval for messages in private window and chat room
+            $interval(function () { $scope.getNewMessages();  }, $scope.randomInterval);
+            $interval(function () { $scope.getNewMessagesPrivate(); }, $scope.randomInterval);
         });
         
-        //Show images for contacts 
+        //Returning image of a user
         $scope.getImage = function (id) {
             $scope.userImg = $scope.usersImages.filter(function (userImg) {
                 return userImg.id === id;
             })[0];
 
             return $scope.userImg.image;
-         
         }
+
+        //Function called in order to return image
         $scope.getImageMessage = function (id, me) {
             if (me === true) {
                 $scope.mediaClass = 'text-left';
@@ -56,13 +61,11 @@
                 $scope.timeClass = 'pull-left';
                 $scope.timeClass2 = 'pull-right';
 
-
-
                 return $scope.getImage(id);
             }
         }
 
-        //Name of user 
+        //Returning name of user
         $scope.getName = function (id, me) {
             if (me === true) {
                 return 'you'
@@ -80,7 +83,7 @@
         //Open/close private windows for messages
         $scope.isCollapsed = true;
         $scope.changeCollapsed = function (id) {
-            if (id == 0) {
+            if (id === 0) {
                 $scope.isCollapsed = !$scope.isCollapsed;
                 $scope.id = 0;
             }
@@ -96,6 +99,15 @@
                 $scope.msg = $scope.allMessages.filter(function (msg) {
                     return msg.id === id;
                 });
+                $scope.ind = '';
+                for (var i = 0; i < $scope.unreadMessages.length ; i++) {
+                    if ($scope.unreadMessages[i].id === id) {
+                        $scope.ind = i;
+                        break;
+                    }
+                }
+                $scope.unreadMessages[i].cou = 0;
+
             }
         }
     
@@ -108,13 +120,13 @@
             $scope.message = '';
 
         }
+
         $scope.respondMessage = function () {
             var lng = $scope.messages.length
             var x = Math.floor((Math.random() * lng) + 1);
             $scope.allMessages.push({ message: $scope.messages[x], id: $scope.id, me: false });
             $scope.msg.push({ message: $scope.messages[x], id: $scope.id, me: false });
             
-            $scope.header = $scope.allMessages[1].message;
         }
 
         //Generating messages for chat room
@@ -126,6 +138,35 @@
             var y = Math.floor((Math.random() * lng2) + 1);
             $scope.otherMessages.push({ message: $scope.messages[x], id: y, me: false });
         };
+
+        //Generating messages for private window /unread
+        $scope.getNewMessagesPrivate = function () {
+
+            var lng = $scope.messages.length;
+            var lng2 = $scope.users.length;
+            var x = Math.floor((Math.random() * lng) + 1);
+            var y = Math.floor((Math.random() * lng2) + 1);
+            $scope.allMessages.push({ message: $scope.messages[x], id: y, me: false });
+            $scope.ind='';
+            for (var i = 0; i < $scope.unreadMessages.length ; i++) {
+                if ($scope.unreadMessages[i].id === y) {
+                    $scope.ind = i;
+                    break;
+                }
+            }
+            var z = $scope.unreadMessages[$scope.ind].cou + 1;
+            $scope.unreadMessages[$scope.ind].cou = z;
+
+        };
+        //Getting count of unread messages for each user
+        $scope.getUnreadCount =function(id){
+            $scope.um = $scope.unreadMessages.filter(function (um) {
+                return um.id === id;
+            })[0].cou;
+     
+            return $scope.um;
+        }
+
         //My message sent in chat room
         $scope.sendMessageRoom = function () {
             $scope.otherMessages.push({ message: $scope.message2, id: $scope.id, me: true });
